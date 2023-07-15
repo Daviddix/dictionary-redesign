e15c388c6e6c5c40ba13eefe3352cf8ae3fbb352
Emmanuel Nsikan-David <en56434@gmail.com>
2023-07-15 01:18:47 +0100

    file and folder structure

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className='homepage-main'>
+      <header>
+        <div className="left">
+          <h1>Dictionary</h1>
+          <p>get definition to words</p>
+        </div>
+
+        <button>
+          
+        </button>
+      </header>
+    </main>
+  )
+}
+
+export default App
