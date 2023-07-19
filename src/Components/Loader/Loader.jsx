f87ddcd9dc6ab495f3d6bf6d31088999cf1b577b
Emmanuel Nsikan-David <en56434@gmail.com>
2023-07-19 11:02:51 +0100

    fixed pos overflow, fixed error comp, added loader

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -2,7 +2,26 @@ import './Loader.css'
 
 function Loader() {
   return (
-    <div className='loader-overlay'><p>Loading definition</p></div>
+  <div className="loader">
+	<div className="loader-inner">
+		<div className="loader-line-wrap">
+			<div className="loader-line"></div>
+		</div>
+		<div className="loader-line-wrap">
+			<div className="loader-line"></div>
+		</div>
+		<div className="loader-line-wrap">
+			<div className="loader-line"></div>
+		</div>
+		<div className="loader-line-wrap">
+			<div className="loader-line"></div>
+		</div>
+		<div className="loader-line-wrap">
+			<div className="loader-line"></div>
+		</div>
+	</div>
+  <p>Loading definition</p>
+</div>
   )
 }
 
